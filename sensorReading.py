2d51dc9cc45ac033727a2c61af91b893d7e10333
justynlgh <34696992+justynlgh@users.noreply.github.com>
2017-12-20 17:22:15 +0800

    minor clean up

--- sensorReading.py
+++ sensorReading.py
@@ -4,15 +4,13 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-pins = (23, 24, 17, 27, 22, 5, 6, 13, 19, 26)
-
 for pin in sensorGlobal.pinArraypins:
     GPIO.setup(pin, GPIO.IN)
 
 
-def pin_triggered(pin):
-    pin_no = sensorGlobal.pinArray.index(pin)
-    sensorGUI.sums[pin_no].set(sensorGUI.sums[pin_no].get() + 1)
+def pin_triggered(port):
+    pin_no = sensorGlobal.pinArray.index(port)
+    sensorGUI.count[pin_no].set(sensorGUI.sums[pin_no].get() + 1)
 
 
 if __name__ == '__main__':
@@ -20,6 +18,7 @@ if __name__ == '__main__':
         for pin in sensorGlobal.pinArray:
             GPIO.add_event_detect(pin, GPIO.RISING, callback=pin_triggered)
 
+        dataHandler = sensorGlobal.DataHandler()
         sensorGUI.start_gui()
 
     except KeyboardInterrupt:
