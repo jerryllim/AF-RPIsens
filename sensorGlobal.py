c5b555cd0d1860cd8724a62898e46f33bf1b7e66
justynlgh <34696992+justynlgh@users.noreply.github.com>
2017-12-22 08:46:34 +0800

    Merge with local repo

--- sensorGlobal.py
+++ sensorGlobal.py
@@ -6,7 +6,7 @@ class DataHandler:
     sensorDict = OrderedDict()
     countDict = {}
 
-    def __init__(self, file_name='testFile.json'):  # TODO change default file name
+    def __init__(self, file_name='testFile.txt'):  # TODO change default file name
         self.fileName = file_name
         self.load_data()
         self.init_count()
@@ -16,8 +16,11 @@ class DataHandler:
             json.dump(self.sensorDict, outfile)
 
     def load_data(self):
-        with open(self.fileName, 'r') as infile:
-            self.sensorDict = json.load(infile, object_pairs_hook=OrderedDict)
+        try:
+            with open(self.fileName, 'r') as infile:
+                self.sensorDict = json.load(infile, object_pairs_hook=OrderedDict)
+        except FileNotFoundError:
+            pass
 
     def get_pins(self):
         return list(self.sensorDict.keys())
@@ -33,11 +36,11 @@ class DataHandler:
 if __name__ == '__main__':
     dataHandler = DataHandler()
 
-    if False:
+    if True:
         tempDict1 = {"Sensor 1": 23, "Sensor 2": 24, "Sensor 3": 17, "Sensor 4": 27, "Sensor 5": 22, "Sensor 6": 5,
                      "Sensor 7": 6, "Sensor 8": 12, "Sensor 9": 19, "Sensor 10": 26}
         tempDict2 = OrderedDict()
-        for name, pin in tempDict1():
+        for name, pin in tempDict1.items():
             tempDict2[pin] = name
 
         dataHandler.sensorDict.clear()
@@ -47,5 +50,6 @@ if __name__ == '__main__':
         print(dataHandler.get_names())
         print(dataHandler.get_pins())
 
-    for pin in dataHandler.countDict.keys():
-        print('{} is of type {}'.format(pin, type(pin)))
+    if False:
+        for pin in dataHandler.countDict.keys():
+            print('{} is of type {}'.format(pin, type(pin)))
