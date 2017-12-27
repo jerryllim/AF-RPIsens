6dfb40ce7b20d4c196fa36972dde89bb9c00a868
Justyn Lim <justynlgh@gmail.com>
2017-12-27 08:56:12 +0800

    added runScript

---
+++ runScript.py
@@ -0,0 +1,7 @@
+import tkinter
+import sensor.sensorReading as sensorReading
+
+
+if __name__ == '__main__':
+    Root = tkinter.Tk()  # TODO try script
+    sensorReading.RaspberryPiController(Root)
