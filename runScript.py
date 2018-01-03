6d5f8311221ed2f6c16aeeb5f50a8001c6953d5f
Justyn Lim <justynlgh@gmail.com>
2018-01-03 09:49:25 +0800

    Added start pigpiod to runScript

--- runScript.py
+++ runScript.py
@@ -1,7 +1,9 @@
+import os
 import tkinter
 import sensor.sensorReading as sensorReading
 
 
 if __name__ == '__main__':
+    os.system("sudo pigpiod")
     Root = tkinter.Tk()
     sensorReading.RaspberryPiController(Root)
