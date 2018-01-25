0afff851f5671bbcfdda08ec74fc472c2091e2ce
Justyn Lim <justynlgh@gmail.com>
2018-01-25 13:30:17 +0800

    improved server GUI

--- matplotlib_server.py
+++ matplotlib_server.py
@@ -13,10 +13,12 @@ class TempClassWithRandomData:  # TODO to delete for testing
     def __init__(self):
         self.sensorList = []
         for index in range(13):
-            self.sensorList.append('Machine {}'.format(string.ascii_uppercase[index]))
+            self.sensorList.append('Machine {}\n1 Jan 2018 Morning'.format(string.ascii_uppercase[index]))
 
 
 class MainWindow(ttk.Frame):
+    NUM_COL = 2
+
     def __init__(self, parent, data_class: TempClassWithRandomData, **kwargs):
         self.data_class = data_class
         ttk.Frame.__init__(self, parent, **kwargs)
@@ -30,23 +32,27 @@ class MainWindow(ttk.Frame):
         bottom_scrollable_frame = VerticalScrollFrame(self)
         bottom_scrollable_frame.grid(row=1, column=0, sticky='nsew')
 
-        # Loop to place all graphs
-        bottom_scrollable_frame.get_interior_frame().columnconfigure(0, weight=1)
-        bottom_scrollable_frame.get_interior_frame().columnconfigure(1, weight=1)
-        # graph = GraphFrameTemp(bottom_scrollable_frame.get_interior_frame())
-        # graph.pack()
-        graph = GraphFrame(bottom_scrollable_frame.get_interior_frame())
-        graph.pack(fill=tkinter.BOTH, expand=tkinter.TRUE)
-        for sensor in self.data_class.sensorList:
-            graph.add_subplots(lambda: temp_get_data(title=sensor))
-        graph.show_figure()
+        # Make graphs
+        for col in range(MainWindow.NUM_COL):
+            bottom_scrollable_frame.get_interior_frame().columnconfigure(col, weight=1)
+
+        for index in range(len(self.data_class.sensorList)):
+            row = index//MainWindow.NUM_COL
+            col = index%MainWindow.NUM_COL
+            # graph = GraphFrame(bottom_scrollable_frame.get_interior_frame(),
+            #                    lambda: temp_get_data(title=self.data_class.sensorList[index]))
+            # graph.grid(row=row, column=col, sticky='nsew', padx=(0, 5), pady=5)
+            canvas = GraphCanvas(bottom_scrollable_frame.get_interior_frame(),
+                                 lambda: temp_get_data(title=self.data_class.sensorList[index]))
+            canvas.show()
+            canvas.grid(row=row, column=col, sticky='nsew', padx=5, pady=5)
 
 
 class VerticalScrollFrame(ttk.Frame):
 
     def __init__(self, parent, **kwargs):
         ttk.Frame.__init__(self, parent, **kwargs)
-        self.canvas = tkinter.Canvas(self, bg='pink')
+        self.canvas = tkinter.Canvas(self)
         self.canvas.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=tkinter.TRUE)
         v_scrollbar = ttk.Scrollbar(self, orient='vertical', command=self.canvas.yview)
         v_scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y, expand=tkinter.FALSE)
@@ -70,70 +76,37 @@ class VerticalScrollFrame(ttk.Frame):
 
 
 class GraphFrame(ttk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, data_func):
         ttk.Frame.__init__(self, parent, relief='ridge', borderwidth=2, width=50)
         self.figure = Figure()
         self.figure.set_tight_layout(True)
-        self.data_funcs = []
+        self.data_func = data_func
 
-    def add_subplots(self, data_func):
-        self.data_funcs.append(data_func)
-
-    def plot_subplot(self, row, col, num, data_func):
-        subplot = self.figure.add_subplot(row, col, num)
-
+        subplot = self.figure.add_subplot(1, 1, 1)
         title, x, y = data_func()
         subplot.plot(x, y, 'b-o')
         subplot.grid(linestyle='dashed')
         subplot.set_title(title)
 
-    def show_figure(self):
-        length = len(self.data_funcs)
-        col = 2
-        row = length//col + length%col
-
-        for pos in range(length):
-            self.plot_subplot(row, col, (pos + 1), self.data_funcs[pos])
-
-        canvas = FigureCanvasTkAgg(figure=self.figure, master=self)
-        canvas.show()
-        canvas.get_tk_widget().pack(fill=tkinter.BOTH, expand=True)
+        self.canvas = FigureCanvasTkAgg(figure=self.figure, master=self)
+        self.canvas.show()
+        self.canvas.get_tk_widget().pack(fill=tkinter.BOTH, expand=tkinter.TRUE)
 
 
-class GraphFrameTemp(ttk.Frame):
-    def __init__(self, parent):
-        ttk.Frame.__init__(self, parent, relief='ridge', borderwidth=2, width=50)
+class GraphCanvas(FigureCanvasTkAgg):
+    def __init__(self, parent, data_func):
         self.figure = Figure()
         self.figure.set_tight_layout(True)
-        self.data_funcs = []
 
-        subplot1 = self.figure.add_subplot(2, 2, 1)
-        title, x, y = temp_get_data()
-        subplot1.plot(x, y, 'b-o')
-        subplot1.grid(linestyle='dashed')
-        subplot1.set_title(title)
+        subplot = self.figure.add_subplot(1, 1, 1)
+        title, x, y = data_func()
+        subplot.plot(x, y, 'b-o')
+        subplot.grid(linestyle='dashed')
+        subplot.set_title(title)
+        FigureCanvasTkAgg.__init__(self, self.figure, parent)
 
-        subplot2 = self.figure.add_subplot(2, 2, 2)
-        title, x, y = temp_get_data()
-        subplot2.plot(x, y, 'b-o')
-        subplot2.grid(linestyle='dashed')
-        subplot2.set_title(title)
-
-        subplot3 = self.figure.add_subplot(2, 2, 3)
-        title, x, y = temp_get_data()
-        subplot3.plot(x, y, 'b-o')
-        subplot3.grid(linestyle='dashed')
-        subplot3.set_title(title)
-
-        subplot4 = self.figure.add_subplot(2, 2, 4)
-        title, x, y = temp_get_data()
-        subplot4.plot(x, y, 'b-o')
-        subplot4.grid(linestyle='dashed')
-        subplot4.set_title(title)
-
-        canvas = FigureCanvasTkAgg(figure=self.figure, master=self)
-        canvas.show()
-        canvas.get_tk_widget().pack(fill=tkinter.BOTH, expand=True)
+    def grid(self, **kwargs):
+        self.get_tk_widget().grid(**kwargs)
 
 
 def temp_get_data(title=None, y=None, x=None):  # TODO change this
@@ -149,26 +122,6 @@ def temp_get_data(title=None, y=None, x=None):  # TODO change this
 
 
 if __name__ == '__main__':
-    # root = tkinter.Tk()
-    # root.title = 'Hello'
-    # root.minsize(width=1000, height=10)
-    # some = VerticalScrollFrame(root)
-    # some.pack(fill=tkinter.BOTH, expand=tkinter.TRUE)
-    # another1 = GraphRowFrame(some.get_interior_frame(), lambda: temp_get_data(title='Machine A - 1 Jan 2018 Morning'))
-    # another1.pack(side=tkinter.TOP, fill=tkinter.X)
-    # another2 = GraphRowFrame(some.get_interior_frame(), lambda: temp_get_data(title='Machine B - 1 Jan 2018 Morning'))
-    # another2.pack(side=tkinter.TOP, fill=tkinter.X)
-    # another3 = GraphRowFrame(some.get_interior_frame(), lambda: temp_get_data(title='Machine C - 1 Jan 2018 Morning'))
-    # another3.pack(side=tkinter.TOP, fill=tkinter.X)
-    # # root.mainloop()
-    # while True:
-    #     try:
-    #         root.mainloop()
-    #     except UnicodeDecodeError:
-    #         continue
-    #     else:
-    #         break
-
     root = tkinter.Tk()
     root.title('Test')
     root.minsize(width=1000, height=100)
