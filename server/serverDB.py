aa55e3ec8c3737c9bd7820743ac178905da91785
Justyn Lim <justynlgh@gmail.com>
2018-01-30 11:39:46 +0800

    minor update

--- server/serverDB.py
+++ server/serverDB.py
@@ -2,29 +2,68 @@ import json
 import zmq
 import logging
 import sqlite3
+from collections import OrderedDict
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+
+class ServerSettings:
+    MACHINE_PORTS = 'machine_ports'
+    QUICK_ACCESS = 'quick_access'
+    MISC_SETTINGS = 'misc_settings'
+
+    def __init__(self, filename='server_settings.json'):
+        self.filename = filename
+        self.logger = logging.getLogger('afRPIsens_server')
+        self.machine_ports = OrderedDict()
+        self.quick_access = OrderedDict()
+        self.misc_settings = {}
+        self.load_settings()
+        self.logger.debug('Completed setup')
+
+    def save_settings(self):
+        self.logger.debug('Saving settings')
+        settings_dict = {ServerSettings.MACHINE_PORTS: self.machine_ports,
+                         ServerSettings.QUICK_ACCESS: self.quick_access,
+                         ServerSettings.MISC_SETTINGS: self.misc_settings}
+        with open(self.filename, 'w') as outfile:
+            json.dump(settings_dict, outfile)
+        self.logger.debug('Saved settings')
+
+    def load_settings(self):
+        self.logger.debug('Loading settings')
+        try:
+            with open(self.filename, 'r') as infile:
+                settings_dict = json.load(infile, object_pairs_hook=OrderedDict)
+                self.machine_ports = settings_dict[ServerSettings.MACHINE_PORTS]
+                self.quick_access = settings_dict[ServerSettings.QUICK_ACCESS]
+                self.misc_settings = settings_dict[ServerSettings.MISC_SETTINGS]
+        except FileNotFoundError:
+            pass
+        self.logger.debug('Loaded settings')
 
 
 class Communication:
-    def __init__(self, filename='portSettings.json'):
+    REQUEST_ID = 'request'
+
+    def __init__(self, server_settings: ServerSettings):
         self.logger = logging.getLogger('afRPIsens_server')
-        self.filename = filename
+        self.server_settings = server_settings
+        self.scheduler = BackgroundScheduler()
         self.ports = []
         self.context = None
         self.socket = None
 
     def req_client(self):
 
-        with open(self.filename, 'r') as infile:
-            machines_port = json.load(infile)
-
-        machines, self.ports = map(list, zip(*machines_port))
+        self.ports = list(self.server_settings.machine_ports.values())
 
         self.context = zmq.Context()
         self.logger.debug("Connecting to the ports")
         self.socket = self.context.socket(zmq.DEALER)
         self.socket.setsockopt(zmq.LINGER, 0)
         for port in self.ports:
-            self.socket.connect("tcp://%s" % port)
+            self.socket.connect("tcp://{}".format(port))
             self.logger.debug("Successfully connected to machine %s" % port)
 
         for request in range(len(self.ports)):
@@ -53,6 +92,11 @@ class Communication:
             else:
                 # TODO machine not respond
                 print("Machine did not respond")
+
+    def set_jobs(self):
+        self.scheduler.remove_all_jobs()
+        cron_trigger = CronTrigger(hour='*', minute='*/1')  # TODO set here
+        self.scheduler.add_job(self.req_client, cron_trigger, id=Communication.REQUEST_ID)
 
 
 class DatabaseManager:
