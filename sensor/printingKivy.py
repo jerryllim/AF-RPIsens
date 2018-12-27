dc24e3dfec694383f58d4d67709bd28fa22b012c
Justyn Lim <justynlgh@gmail.com>
2018-12-27 13:02:43 +0800

    Added Ink Key and minor font changes

--- sensor/printingKivy.py
+++ sensor/printingKivy.py
@@ -1,5 +1,6 @@
 import os
 os.environ['KIVY_GL_BACKEND'] = 'gl'
+import re
 import cv2
 import time
 import json
@@ -19,6 +20,7 @@ from kivy.graphics.texture import Texture
 from kivy.uix.scrollview import ScrollView
 from kivy.properties import NumericProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.graphics import Color, Rectangle
 
 
 class SelectPage(Screen):
@@ -63,9 +65,18 @@ class SelectPage(Screen):
             with open('{}.json'.format(job_num), 'r') as infile:
                 job_dict = json.load(infile)
 
-            self.parent.get_screen('run_page').generate_screen(job_dict)
+            try:
+                fileName = job_dict.get('Code', '').replace('/', '_')
+                with open('{}.json'.format(fileName)) as inkfile:
+                    item_ink_key_dict = json.load(inkfile)
+
+            except FileNotFoundError:
+                item_ink_key_dict = {}
+
+            self.parent.get_screen('run_page').generate_screen(job_dict, item_ink_key_dict)
             self.parent.transition.direction = 'left'
             self.parent.current = 'run_page'
+
         except FileNotFoundError:
             popup_boxlayout = BoxLayout(orientation='vertical')
             popup_boxlayout.add_widget(Label(text='JO number ("{}") was not found, please try again.'.
@@ -83,9 +94,9 @@ class RunPage(Screen):
     def clear_screen(self):
         self.clear_widgets()
 
-    def generate_screen(self, job_dict):
+    def generate_screen(self, job_dict, item_ink_key_dict):
         self.clear_screen()
-        self.add_widget(Factory.RunPageLayout(job_dict))
+        self.add_widget(Factory.RunPageLayout(job_dict, item_ink_key_dict))
 
     def reject_popup(self):
         popup_boxlayout = BoxLayout(orientation='vertical', spacing=10, padding=10)
@@ -102,12 +113,14 @@ class RunPage(Screen):
         self.rejectPopup.dismiss()
         self.parent.transition.direction = 'right'
         self.parent.current = 'select_page'
+        # TODO save info to output for SFU
+        # TODO save ink key info
 
 
 class RunPageLayout(BoxLayout):
     counter_number = NumericProperty(0)
 
-    def __init__(self, job_dict, **kwargs):
+    def __init__(self, job_dict, item_ink_key_dict, **kwargs):
         BoxLayout.__init__(self, **kwargs)
         self.ids['test_button'].state = 'down'
 
@@ -119,10 +132,74 @@ class RunPageLayout(BoxLayout):
         label_length = 40
         self.ids['item_desc'].text = description[0:label_length]
         self.ids['item_desc2'].text = description[label_length:]
+        self.ids['ink_key_tab'].generate_ink_key(item_ink_key_dict)
 
 
 class AdjustmentTab(ScrollView):
     pass
+
+
+class InkKeyTab(ScrollView):
+    def generate_ink_key(self, ink_key_dict):
+        self.clear_widgets()
+        self.add_widget(Factory.InkKeyBoxLayout(ink_key_dict))
+
+
+class InkKeyBoxLayout(BoxLayout):
+    def __init__(self, ink_key_dict, **kwargs):
+        BoxLayout.__init__(self, **kwargs)
+        self.ink_key_dict = ink_key_dict
+        self.ids['impression'].text = '{}'.format(self.ink_key_dict.get('impression', ''))
+        keys = list(self.ink_key_dict.keys())
+        try:
+            keys.remove('impression')
+        except ValueError:
+            pass
+        for key in keys:
+            layout = InkZoneLayout(key, self.ink_key_dict.get(key, ''))
+            self.add_widget(layout)
+
+
+class InkZoneLayout(BoxLayout):
+    def __init__(self, plate, ink_dict, **kwargs):
+        BoxLayout.__init__(self, **kwargs)
+        self.ink_dict = ink_dict
+        self.buttons = []
+        self.ids['plate_code'].text = plate
+        self.load_widgets()
+
+    def load_widgets(self):
+        self.buttons.clear()
+        self.ids['ink_zones'].clear_widgets()
+
+        zones = sorted(self.ink_dict.keys(), key=alphanum_key)
+        for zone in zones:
+            button = Button(text="{}\n[b][size=20sp]{}[/size][/b]".format(zone, self.ink_dict.get(zone, '')),
+                            size_hint=(None, 1), halign='center', markup=True)
+            button.bind(on_press=self.edit_ink_key)
+            self.buttons.append(button)
+            self.ids['ink_zones'].add_widget(button)
+
+    def edit_ink_key(self, instance):
+        def dismiss_popup(_button):
+            if value_textinput.text:
+                self.ink_dict[key] = int(value_textinput.text)
+            else:
+                self.ink_dict[key] = 0
+
+            edit_popup.dismiss()
+            self.load_widgets()
+
+        text = instance.text
+        key, value = text.split('\n', 2)
+        content_boxlayout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+        value_textinput = TextInput()
+        content_boxlayout.add_widget(value_textinput)
+        dismiss_button = Button(text='Dismiss')
+        content_boxlayout.add_widget(dismiss_button)
+        edit_popup = Popup(title='Edit ink key {}'.format(key), content=content_boxlayout, auto_dismiss=False, size_hint=(0.5, 0.5))
+        dismiss_button.bind(on_press=dismiss_popup)
+        edit_popup.open()
 
 
 class AdjustmentTextInput(TextInput):
@@ -142,13 +219,28 @@ class PrintingGUIApp(App):
         Window.bind(on_keyboard=self.on_keyboard)
 
         Factory.register('RunPageLayout', cls=RunPageLayout)
+        Factory.register('InkKeyBoxLayout', cls=InkKeyBoxLayout)
         self.screen_manager.add_widget(SelectPage(name='select_page'))
         self.screen_manager.add_widget(RunPage(name='run_page'))
         return self.screen_manager
 
-    def on_keyboard(self, _window, _key, _scancode, codepoint, modifier):
-        if codepoint == 'Q':
+    def on_keyboard(self, _window, _key, _scan_code, code_point, _modifier):
+        if code_point == 'Q':
             self.stop()
+
+
+def try_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        return s
+
+
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [try_int(c) for c in re.split('([0-9]+)', s)]
 
 
 if __name__ == '__main__':
