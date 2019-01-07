37e6c232e64769f78a2808a6bf1f0f53629c751d
Justyn Lim <justynlgh@gmail.com>
2019-01-07 11:51:06 +0800

    Started on printingMain.py

--- sensor/printingMain.py
+++ sensor/printingMain.py
@@ -1,6 +1,7 @@
+import json
 import pigpio
 import datetime
-import json
+import threading
 from collections import Counter, namedtuple
 
 sensorInfo = namedtuple('sensorInfo', ['pin', 'bounce'])
@@ -9,11 +10,19 @@ sensorInfo = namedtuple('sensorInfo', ['pin', 'bounce'])
 class RaspberryPiController:
     sensor_dict = {}
     pin_to_name = {}
+    counts = {}
 
     def __init__(self, filename='pin_dict.json'):
         self.filename = filename
         self.load_pin_dict()
-        # TODO add threading.lock?
+        self.pi = pigpio.pi()
+        self.callbacks = []
+        self.counts_lock = threading.Lock()
+
+        for name, (pin, bounce) in self.sensor_dict.items():
+            self.pin_setup(pin, bounce)
+
+        # TODO start GUI
 
     def load_pin_dict(self):
         try:
@@ -32,3 +41,23 @@ class RaspberryPiController:
             temp[pin] = name
 
         return temp
+
+    def pin_triggered(self, pin, _level, _tick):
+        name = self.pin_to_name[pin]
+        now = datetime.datetime.utcnow()
+        now = now - datetime.timedelta(minutes=now.minute % 5)
+        datetime_stamp = now.strftime('%Y-%m-%d %H:%M')
+        self.update_count(name, datetime_stamp)
+        # TODO Add update the string in GUI
+
+    def pin_setup(self, pin, bounce=30):
+        self.pi.set_mode(pin, pigpio.INPUT)
+        self.pi.set_pull_up_down(pin, pigpio.PUD_DOWN)
+        self.pi.set_glitch_filter(pin, (bounce * 1000))
+        self.callbacks.append(self.pi.callback(pin, pigpio.RISING_EDGE, self.pin_triggered))
+
+    def update_count(self, name, datetime_stamp):
+        with self.counts_lock:
+            if self.counts.get(name) is None:
+                self.counts[name] = Counter()
+            self.counts[name].update(datetime_stamp)
