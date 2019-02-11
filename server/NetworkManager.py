9ee9d7bc98198e4f169216eee7514e9ba33a5c72
Jerryl <34697037+jerryllim@users.noreply.github.com>
2019-02-11 12:34:06 +0800

    network dealer router

--- server/NetworkManager.py
+++ server/NetworkManager.py
@@ -1,43 +1,86 @@
 import zmq
 import json
 import time
-
+import pymysql
+import threading
+import databaseServer
+import apscheduler.schedulers.background
 
 class NetworkManager:
 	def __init__(self):
 		self.context = zmq.Context()
-		self.requester_routine()
-		self.respondent_routine()
+		self.dealer_routine()
+		self.router_routine()
 
-	def requester_routine(self):
+	def dealer_routine(self):
 		#port_number = "{}:8888".format(self.self_add)
 		port_number = "152.228.1.124:8888"
-		self.requester = self.context.socket(zmq.REQ)
-		self.requester.connect("tcp://%s" % port_number)
-		#print("Successfully connected to machine %s" % port_number)
+		self.dealer = self.context.socket(zmq.DEALER)
+		self.dealer.setsockopt(zmq.LINGER, 0)
+		for port in port_number:
+			self.dealer.connect("tcp://%s" % port_number)
+			#print("Successfully connected to machine %s" % port_number)
 
-	def request(self):
-		self.requester.send_string("request msg")
+	def request(self, msg):
+		msg_json = json.dumps(msg)
+		self.dealer.send_string("", zmq.SNDMORE)  # delimiter
+		self.dealer.send_string(msg_json)
 		print("request msg sent")
-		recv_msg = str(self.requester.recv())
-		print("recv_msg: %s" % recv_msg)
 
-	def respondent_routine(self):
+		# use poll for timeouts:
+	    poller = zmq.Poller()
+	    poller.register(dealer, zmq.POLLIN)
+
+    	socks = dict(poller.poll(3 * 1000))
+
+    	if self.dealer in socks:
+    		try:
+				recv_msg = str(self.dealer.recv())
+				#print("recv_msg: %s" % recv_msg)
+				jam_msg = json.loads(recv_msg)
+				return jam_msg
+			except IOError as error:
+				print("Problem with socket")
+		else:
+			print("Machine did not respond")			
+
+
+	def router_routine(self):
 		#port_number = "{}:9999".format(self.self_add)
 		port_number = "152.228.1.124:9999"
-		self.respondent = self.context.socket(zmq.REP)
-		self.respondent.setsockopt(zmq.LINGER, 0)
-		self.respondent.bind("tcp://%s" % port_number)
+		self.router = self.context.socket(zmq.ROUTER)
+		self.router.bind("tcp://%s" % port_number)
 		#print("Successfully binded to port %s for respondent" % self.port_number)
 
-	def respond(self):
+	def route(self):
 		while True:
-			# wait for next request from client
-			_message = str(self.respondent.recv())
-			# print("Received request (%s)" % _message)
-			res_json = self.get_counts()
-			self.respondent.send_string(res_json)
+			ident = self.router.recv() # routing information
+			delimiter = self.router.recv() # delimiter
+			message = self.router.recv_json()
+			reply_dict = {}
 
+			for keys in message.keys():
+				if key == "job_info":
+					barcode = message.get("job_info", None)
+					reply_dict = databaseServer.get_job(barcode)
+				elif key == "sfu":
+					pass
+				elif key == "ink_key":
+					ink_key = message.get("ink_key", None)
+					databaseServer.get_ink_key(ink_key)
+
+			self.router.send(ident, zmq.SNDMORE)
+			self.router.send(delimiter, zmq.SNDMORE)
+			self.router.send_json(reply_dict)
+
+	def request_jam(self):
+		msg_dict = {}
+		self.request(msg_dict)
+
+	def request_schedule(self):
+		scheduler = apscheduler.schedulers.background.BackgroundScheduler()
+		self.job = scheduler.add_job(self.request_jam, 'cron', minute='*/5', id='5mins')
+		scheduler.start()
 
 if __name__ == '__main__':
 	NetworkManager()
