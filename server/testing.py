1602d170884fca1ea3a9de1bd1f18368aa29124c
Jerryl Lim <jerryl@Jerryls-MacBook-Pro.local>
2019-02-16 09:48:19 +0800

    added exit option and try,except,finally clause

--- server/testing.py
+++ server/testing.py
@@ -1,10 +1,21 @@
+import sys
 import NetworkManager
 
 network_manager = NetworkManager.NetworkManager()
 
-while True:
-	jam = input("option 1: jam\n")
-	if jam == "1":
-		network_manager.request_jam()
-	else:
-		print("no such commands")
+try:
+	while True:
+		jam = input("\noption 1: jam\noption 2: exit\nplease enter the option no.\n")
+		if jam == "1":
+			print("requesting...")
+			network_manager.request_jam()
+		elif:
+			print("exiting...")
+			break
+		else:
+			print("no such commands")
+except KeyboardInterrupt:
+	print("interrupted with ctrl+c")
+	raise
+finally:
+	sys.exit()
