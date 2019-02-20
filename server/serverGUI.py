9f415c051905bf9aa8e4543b2f854a0adfe3560e
Jerryl Lim <jerryl@Jerryls-MacBook-Pro.local>
2019-02-20 17:44:08 +0800

    sensor layout and error

--- server/serverGUI.py
+++ server/serverGUI.py
@@ -29,9 +29,6 @@ class Network(ttk.Frame):
 		self.details_frame()
 
 	def treeview_frame(self):
-		# label = ttk.Label(self, text="Hi, this is network")
-		# label.grid(row=1,column=0,padx=10,pady=10)
-
 		self.topFrame = tk.Frame(self, background='red')
 		self.topFrame.pack(side=tk.TOP, fill=tk.BOTH, anchor=tk.N)
 
@@ -45,20 +42,18 @@ class Network(ttk.Frame):
 		terminal_tree.configure(yscrollcommand=terminal_scrollbar.set)
 		self.topFrame.columnconfigure(2, weight=1)
 		self.topFrame.rowconfigure(2, weight=1)
-		terminal_tree["columns"] = ("1", "2", "3")
+		terminal_tree["columns"] = ("1", "2")
 		terminal_tree['show'] = 'headings'
 		terminal_tree.column("1", width=200, anchor='c')
 		terminal_tree.column("2", width=200, anchor='c')
-		terminal_tree.column("3", width=200, anchor='c')
 		terminal_tree.heading("1", text="Machine")
 		terminal_tree.heading("2", text="IP")
-		terminal_tree.heading("3", text="Date Modified")
 
 	def button_frame(self):
 		self.midFrame = tk.Frame(self, background='green')
 		self.midFrame.pack(side=tk.TOP, fill=tk.BOTH, anchor=tk.N)
 		# buttons
-		add_button = ttk.Button(self.midFrame, text="Add")
+		add_button = ttk.Button(self.midFrame, text="Add", command=self.get_entry_dict)
 		add_button.grid(row=0, column=0, padx=10, pady=5, sticky=tk.EW)
 
 		edit_button = ttk.Button(self.midFrame, text="Edit")
@@ -83,176 +78,76 @@ class Network(ttk.Frame):
 			self.bottomFrame.rowconfigure(rows, weight=1)
 
 		# terminal input label and entry
-		Machine_label = ttk.Label(self.bottomFrame, text="Machine: ")
+		Machine_label = ttk.Label(self.bottomFrame, text="Machine")
 		Machine_label.grid(row=0, column=0, padx=(40,0), pady=5, sticky=tk.E)
 
-		Machine_entry = ttk.Entry(self.bottomFrame, text="Machine")
-		Machine_entry.grid(row=0, column=1, padx=(0,50), pady=5, sticky=tk.W)
+		self.Machine_entry = ttk.Entry(self.bottomFrame, text="Machine")
+		self.Machine_entry.grid(row=0, column=1, padx=(0,50), pady=5, sticky=tk.W)
 
-		IP_label = ttk.Label(self.bottomFrame, text="IP: ")
+		IP_label = ttk.Label(self.bottomFrame, text="IP")
 		IP_label.grid(row=0, column=3, pady=5, sticky=tk.E)
 
-		IP_entry = ttk.Entry(self.bottomFrame, text="IP")
-		IP_entry.grid(row=0, column=4, padx=(0,50), pady=5, sticky=tk.W)
+		self.IP_entry = ttk.Entry(self.bottomFrame, text="IP")
+		self.IP_entry.grid(row=0, column=4, padx=(0,50), pady=5, sticky=tk.W)
 
-		Mac_label = ttk.Label(self.bottomFrame, text="Mac: ")
+		Mac_label = ttk.Label(self.bottomFrame, text="Mac")
 		Mac_label.grid(row=1, column=0, pady=5, sticky=tk.E)
 
-		Mac_entry = ttk.Entry(self.bottomFrame, text="Mac")
-		Mac_entry.grid(row=1, column=1, pady=5, sticky=tk.W)
+		self.Mac_entry = ttk.Entry(self.bottomFrame, text="Mac")
+		self.Mac_entry.grid(row=1, column=1, pady=5, sticky=tk.W)
 
 		Separator = ttk.Separator(self.bottomFrame, orient='horizontal')
 		Separator.grid(row=2, columnspan=5, pady=5, sticky=tk.EW)
 
-		sensorOddList = ["S01", "S03", "S05", "S07", "SO9", "S11", "S13", "S15", "E02", "E04"]
-		sensorEvenList = ["S02", "S04", "S06", "S08", "S10", "S12", "S14", "E01", "E03", "E05"]
-
+		sensorOddList = ["S01", "S02", "S03", "S04", "SO5", "S06", "S07", "S08", "S09", "S10"]
+		sensorEvenList = ["S11", "S12", "S13", "S14", "S15", "E01", "E02", "E03", "E04", "E05"]
+		self.sensorEntry = []
+		sensorLabel = []
 		rowOdd = 3
 		rowEven = 3
 
-		for sensor in range(len(sensorOddList)):
+		for sensor in range(0, len(sensorOddList)):
 			rowOdd = rowOdd + 1
-			columnLabel = 0
-			columnEntry = 1
-			sensorLabel = sensorOddList[sensor] + "_label"
-			sensorLabel = ttk.Label(self.bottomFrame, text="%s: " % sensorOddList[sensor])
-			sensorLabel.grid(row=rowOdd, column=columnLabel, pady=5, sticky=tk.E)
+			sensorLabel.append(ttk.Label(self.bottomFrame, text=sensorOddList[sensor]))
+			self.sensorEntry.append(ttk.Entry(self.bottomFrame, text=sensorOddList[sensor]))
+			sensorLabel[-1].grid(row=rowOdd, column=0, pady=5, sticky=tk.E)
+			self.sensorEntry[-1].grid(row=rowOdd, column=1, pady=5, sticky=tk.W)
 
-			sensorEntry = sensorOddList[sensor] + "_entry"
-			sensorEntry = ttk.Entry(self.bottomFrame, text=sensorOddList[sensor])
-			sensorEntry.grid(row=rowOdd, column=columnEntry, pady=5, sticky=tk.W)
-
-		for sensor in range(len(sensorEvenList)):
+		for sensor in range(0, len(sensorEvenList)):
 			rowEven = rowEven + 1
-			columnLabel = 3
-			columnEntry = 4
-			sensorLabel = sensorEvenList[sensor] + "_label"
-			sensorLabel = ttk.Label(self.bottomFrame, text="%s: " % sensorEvenList[sensor])
-			sensorLabel.grid(row=rowEven, column=columnLabel, pady=5, sticky=tk.E)
+			sensorLabel.append(ttk.Label(self.bottomFrame, text=sensorEvenList[sensor]))
+			self.sensorEntry.append(ttk.Entry(self.bottomFrame, text=sensorEvenList[sensor]))
+			sensorLabel[-1].grid(row=rowEven, column=3, pady=5, sticky=tk.E)
+			self.sensorEntry[-1].grid(row=rowEven, column=4, pady=5, sticky=tk.W)
 
-			sensorEntry = sensorEvenList[sensor] + "_entry"
-			sensorEntry = ttk.Entry(self.bottomFrame, text=sensorEvenList[sensor])
-			sensorEntry.grid(row=rowEven, column=columnEntry, pady=5, sticky=tk.W)
+	def get_entry_dict(self):
+		self.setting_dict = {}
+		sensor_dict = {}
+		if (self.IP_entry.get() and self.Machine_entry.get() and self.Mac_entry.get()):
+			sensor_dict.update({self.Machine_entry.cget("text"): self.Machine_entry.get() or None})
+			sensor_dict.update({self.Mac_entry.cget("text"): self.Mac_entry.get() or None})
 
-		# S01_label = ttk.Label(self.bottomFrame, text="S01: ")
-		# S01_label.grid(row=3, column=0, sticky=tk.E)
+			for sensor in range(len(self.sensorEntry)):
+				sensor_dict.update({self.sensorEntry[sensor].cget("text"): self.sensorEntry[sensor].get() or None})
 
-		# S01_entry = ttk.Entry(self.bottomFrame, text="S01")
-		# S01_entry.grid(row=3, column=1, sticky=tk.W)
+			self.setting_dict[self.IP_entry.get()] = sensor_dict
 
-		# S02_label = ttk.Label(self.bottomFrame, text="S02: ")
-		# S02_label.grid(row=4, column=0, sticky=tk.E)
+			print(self.setting_dict)
+		else:
+			print("Please input the mandatory fields")
+			popup = tk.Toplevel(self.master)
+			popup.title("Error")
+			popup.transient(self.master)
+			popup.grab_set()
+			# error label
+			error_label = ttk.Label(popup, text="Please input the mandatory fields: \nMachine, IP and Mac")
+			error_label.grid(row=0, padx=5)
+			# OK button
+			ok_button = ttk.Button(popup, text="Okay", command=popup.destroy)
+			ok_button.grid(row=1, pady=10)
 
-		# S02_entry = ttk.Entry(self.bottomFrame, text="S02")
-		# S02_entry.grid(row=4, column=1, sticky=tk.W)
-
-		# S03_label = ttk.Label(self.bottomFrame, text="S03: ")
-		# S03_label.grid(row=5, column=0, sticky=tk.E)
-
-		# S03_entry = ttk.Entry(self.bottomFrame, text="S03")
-		# S03_entry.grid(row=5, column=1, sticky=tk.W)
-
-		# S04_label = ttk.Label(self.bottomFrame, text="S04: ")
-		# S04_label.grid(row=6, column=0, sticky=tk.E)
-
-		# S04_entry = ttk.Entry(self.bottomFrame, text="S04")
-		# S04_entry.grid(row=6, column=1, sticky=tk.W)
-
-		# S05_label = ttk.Label(self.bottomFrame, text="S05: ")
-		# S05_label.grid(row=7, column=0, sticky=tk.E)
-
-		# S05_entry = ttk.Entry(self.bottomFrame, text="S05")
-		# S05_entry.grid(row=7, column=1, sticky=tk.W)
-
-		# S06_label = ttk.Label(self.bottomFrame, text="S06: ")
-		# S06_label.grid(row=8, column=0, sticky=tk.E)
-
-		# S06_entry = ttk.Entry(self.bottomFrame, text="S06")
-		# S06_entry.grid(row=8, column=1, sticky=tk.W)
-
-		# S07_label = ttk.Label(self.bottomFrame, text="S07: ")
-		# S07_label.grid(row=9, column=0, sticky=tk.E)
-
-		# S07_entry = ttk.Entry(self.bottomFrame, text="S07")
-		# S07_entry.grid(row=9, column=1, sticky=tk.W)
-
-		# S08_label = ttk.Label(self.bottomFrame, text="S08: ")
-		# S08_label.grid(row=10, column=0, sticky=tk.E)
-
-		# S08_entry = ttk.Entry(self.bottomFrame, text="S08")
-		# S08_entry.grid(row=10, column=1, sticky=tk.W)
-
-		# S09_label = ttk.Label(self.bottomFrame, text="S09: ")
-		# S09_label.grid(row=11, column=0, sticky=tk.E)
-
-		# S09_entry = ttk.Entry(self.bottomFrame, text="S09")
-		# S09_entry.grid(row=11, column=1, sticky=tk.W)
-
-		# S10_label = ttk.Label(self.bottomFrame, text="S10: ")
-		# S10_label.grid(row=12, column=0, sticky=tk.E)
-
-		# S10_entry = ttk.Entry(self.bottomFrame, text="S10")
-		# S10_entry.grid(row=12, column=1, sticky=tk.W)
-
-		# S11_label = ttk.Label(self.bottomFrame, text="S11: ")
-		# S11_label.grid(row=3, column=3, sticky=tk.E)
-
-		# S11_entry = ttk.Entry(self.bottomFrame, text="S11")
-		# S11_entry.grid(row=3, column=4, sticky=tk.W)
-
-		# S12_label = ttk.Label(self.bottomFrame, text="S12: ")
-		# S12_label.grid(row=4, column=3, sticky=tk.E)
-
-		# S12_entry = ttk.Entry(self.bottomFrame, text="S12")
-		# S12_entry.grid(row=4, column=4, sticky=tk.W)
-
-		# S13_label = ttk.Label(self.bottomFrame, text="S13: ")
-		# S13_label.grid(row=5, column=3, sticky=tk.E)
-
-		# S13_entry = ttk.Entry(self.bottomFrame, text="S13")
-		# S13_entry.grid(row=5, column=4, sticky=tk.W)
-
-		# S14_label = ttk.Label(self.bottomFrame, text="S14: ")
-		# S14_label.grid(row=6, column=3, sticky=tk.E)
-
-		# S14_entry = ttk.Entry(self.bottomFrame, text="S14")
-		# S14_entry.grid(row=6, column=4, sticky=tk.W)
-
-		# S15_label = ttk.Label(self.bottomFrame, text="S15: ")
-		# S15_label.grid(row=7, column=3, sticky=tk.E)
-
-		# S15_entry = ttk.Entry(self.bottomFrame, text="S15")
-		# S15_entry.grid(row=7, column=4, sticky=tk.W)
-
-		# E01_label = ttk.Label(self.bottomFrame, text="E01: ")
-		# E01_label.grid(row=8, column=3, sticky=tk.E)
-
-		# E01_entry = ttk.Entry(self.bottomFrame, text="E01")
-		# E01_entry.grid(row=8, column=4, sticky=tk.W)
-
-		# E02_label = ttk.Label(self.bottomFrame, text="E02: ")
-		# E02_label.grid(row=9, column=3, sticky=tk.E)
-
-		# E02_entry = ttk.Entry(self.bottomFrame, text="E02")
-		# E02_entry.grid(row=9, column=4, sticky=tk.W)
-
-		# E03_label = ttk.Label(self.bottomFrame, text="E03: ")
-		# E03_label.grid(row=10, column=3, sticky=tk.E)
-
-		# E03_entry = ttk.Entry(self.bottomFrame, text="E03")
-		# E03_entry.grid(row=10, column=4, sticky=tk.W)
-
-		# E04_label = ttk.Label(self.bottomFrame, text="E04: ")
-		# E04_label.grid(row=11, column=3, sticky=tk.E)
-
-		# E04_entry = ttk.Entry(self.bottomFrame, text="E04")
-		# E04_entry.grid(row=11, column=4, sticky=tk.W)
-
-		# E05_label = ttk.Label(self.bottomFrame, text="E05: ")
-		# E05_label.grid(row=12, column=3, sticky=tk.E)
-
-		# E05_entry = ttk.Entry(self.bottomFrame, text="E05")
-		# E05_entry.grid(row=12, column=4, sticky=tk.W)
+			popup.rowconfigure(0, weight=1)
+			popup.columnconfigure(0, weight=1)
 
 class Extra(ttk.Frame):
 	def __init__(self, master):
